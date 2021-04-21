f6a3ebcd079bee2c7509db7614d7e79e3e2f1c3f
Shubham <shubhamsunil.phape@mavs.uta.edu>
2021-04-21 16:31:50 -0500

    intiss

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/waytodashboard/<mail>', methods=['GET', 'POST'])
+def waytodashboard(mail):
+    
+    print(mail)
+    return render_template('dashboard.html', useremail = mail)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
