5eddfffaef86367bd3583ac5b19848283da6f867
Shubham <shubhamsunil.phape@mavs.uta.edu>
2021-04-21 17:52:57 -0500

    routes

--- app.py
+++ app.py
@@ -12,13 +12,27 @@ def waytodashboard(mail):
     
     print(mail)
     return render_template('dashboard.html', useremail = mail)
-    
+
 
 @app.route('/hash/<mail>', methods = ['GET', 'POST'])
 def hash(mail):
 
     print(mail)
-    return mail
+    return     render_template('hash.html', useremail = mail)
+
+
+@app.route('/aes/<mail>', methods = ['GET', 'POST'])
+def aes(mail):
+
+    print(mail)
+    return render_template('aes.html', useremail = mail)
+
+
+@app.route('/rsa/<mail>', methods = ['GET', 'POST'])
+def rsa(mail):
+
+    print(mail)
+    return     render_template('rsa.html', useremail = mail)
 
 
 if __name__ == '__main__':
