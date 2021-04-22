ab26f95ff299829c6e6cf24e73cf9ae4b32869ba
Shubham <shubhamsunil.phape@mavs.uta.edu>
2021-04-22 02:34:10 -0500

    feef

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify, make_response
-# from cryptography import Fernet
+from cryptography.fernet import Fernet
 
+
+#References: https://www.thepythoncode.com/article/encrypt-decrypt-files-symmetric-python
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -12,42 +15,54 @@ def index():
 @app.route('/waytodashboard/<mail>', methods=['GET', 'POST'])
 def waytodashboard(mail):
     
-    print(mail)
     return render_template('dashboard.html', useremail = mail)
 
 
 @app.route('/hash/<mail>', methods = ['GET', 'POST'])
 def hash(mail):
 
-    print(mail)
     return     render_template('hash.html', useremail = mail)
 
 
 @app.route('/aes/<mail>', methods = ['GET', 'POST'])
 def aes(mail):
 
-    print(mail)
     return render_template('aes.html', useremail = mail)
 
 
 @app.route('/rsa/<mail>', methods = ['GET', 'POST'])
 def rsa(mail):
 
-    print(mail)
     return     render_template('rsa.html', useremail = mail)
 
 
 @app.route('/generateonekey', methods = ['GET', 'POST'])
 def generateonekey():
-   # return Fernet.generate_key()
     req = request.get_json()
-
-    print(req)
-
-    res = make_response(jsonify({"onekey": "OK"}), 200)
+    key = Fernet.generate_key()
+    hex_str = key.hex()
+    new_rnd_bytes = bytes.fromhex(hex_str)
+    
+    res = make_response(jsonify({"message": hex_str}), 200)
     return res
 
-
+@app.route('/onekeyhashfile', methods = ['POST', 'GET'])
+def onekeyhashfile():
+    if (request.method == 'POST'):
+        file = request.files['file']
+        encryptionkey = request.form.get('aes_key')
+        
+        new_rnd_bytes = bytes.fromhex(encryptionkey)
+        
+        output_filename = "/encrypted/"+file.filename
+        fernet = Fernet(new_rnd_bytes)
+        encrypted_data = fernet.encrypt(file.read())
+        print(new_rnd_bytes)
+    
+        f = open(output_filename,"wb")
+        f.write(encrypted_data)
+        f.close()
+    return output_filename
 
 if __name__ == '__main__':
     app.run()
